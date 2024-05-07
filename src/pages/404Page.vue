2a4e2f6767e19bab752f89b8bfb53cd8d9406ac9
Nuri Akman <nuriakman@gmail.com>
2024-05-07 13:45:57 +0300

    404 Page sayfası eklendi

---
+++ src/pages/404Page.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
+  >
+    <div>
+      <div style="font-size: 30vh">404</div>
+
+      <div class="text-h2" style="opacity: 0.4">Sayfa bulunamadı...</div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="blue"
+        unelevated
+        to="/"
+        label="Ana Sayfa"
+        no-caps
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
