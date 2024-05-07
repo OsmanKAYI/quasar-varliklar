7fbacf209f0e2b335fa50522ee8a8db2ac2dc695
Nuri Akman <nuriakman@gmail.com>
2024-05-07 13:43:55 +0300

    route tanımları yapıldı

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,14 +4,32 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('src/pages/IndexPage.vue') },
+      {
+        path: 'login',
+        component: () => import('src/pages/LoginPage.vue'),
+      },
+      {
+        path: 'about',
+        component: () => import('src/pages/AboutPage.vue'),
+      },
+      {
+        path: 'asset-types',
+        component: () => import('src/pages/AssetTypesPage.vue'),
+      },
+      {
+        path: 'assets',
+        component: () => import('src/pages/AssetsPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    component: () => import('src/pages/404Page.vue'),
   },
 ];
 
