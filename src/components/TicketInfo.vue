e3a54318d659f8343ed265872faf3bb6515a8661
Nuri Akman <nuriakman@gmail.com>
2024-06-13 17:59:15 +0300

    TicketInfo bileşeni eklendi

---
+++ src/components/TicketInfo.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-list bordered class="rounded-borders" style="max-width: 400px">
+      <q-item>
+        <q-item-section class="col-4">
+          <q-img
+            src="https://s3.eu-central-1.amazonaws.com/static.obilet.com/images/partner/2231-sm.png"
+          />
+        </q-item-section>
+
+        <q-item-section class="col-8" side>
+          <q-item-label header class="text-subtitle1 text-weight-light"
+            >Özlem Seyahat
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Kalkış:</q-item-label>
+          <q-item-label caption>ANKARA</q-item-label>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Varış:</q-item-label>
+          <q-item-label caption>ÇANKIRI</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Hareket Zamanı:</q-item-label>
+          <q-item-label caption>13 Mayıs 2024, Saat: 16:00</q-item-label>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Koltuk No:</q-item-label>
+          <q-item-label caption>27</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Bilet İşlemleri:</q-item-label>
+          <q-item-label caption
+            >Biletiniz açığa alınamaz, değiştirilemez veya iptal edilemez.
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
