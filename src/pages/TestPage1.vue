561ac2402cfc12b32123810c56e0e2f965f55644
Nuri Akman <nuriakman@gmail.com>
2024-06-29 06:14:12 +0300

    İstasyonn seçim ekranı optimize edildi

--- src/pages/TestPage1.vue
+++ src/pages/TestPage1.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card class="q-ma-sm">
+  <div class="dialog-content q-ma-none bg-white">
     <q-toolbar class="bg-primary text-white">
       <q-avatar>
         <img src="/logos/OzlemLogo_64x64.svg" />
@@ -8,61 +8,58 @@
       <q-btn flat round dense icon="close" size="lg" v-close-popup />
     </q-toolbar>
 
-    <q-card class="q-pa-none q-ma-none">
+    <q-card class="first-card q-pa-none q-ma-none q-my-md">
       <!-- ################## Input Filter ##################### -->
       <q-card-section class="q-px-md q-ma-none">
         <q-input
           v-model="myFilter"
+          type="search"
           filled
           outlined
           debounce="0"
-          placeholder="Bulmak için buraya yazabilirsiniz"
+          placeholder="Arayınız..."
           clearable
-          :style="`font-size: ${fontSize}px`"
+          :class="{ 'big-text': myFilter }"
+          ref="quasarSearchInput"
         >
           <template v-slot:prepend>
-            <q-icon name="location_on" size="md" color="primary" />
+            <q-icon name="search" size="lg" color="primary" />
           </template>
         </q-input>
       </q-card-section>
     </q-card>
 
     <!-- ################## Filtered Items ##################### -->
-    <q-card class="q-pa-none q-ma-none">
+
+    <q-card class="second-card q-pa-none q-ma-none" :bordered="false">
       <q-card-section v-if="filteredOptions.length == 0">
         <div class="text-h6">Aradığınız yer bulunamadı</div>
       </q-card-section>
 
-      <q-scroll-area
-        :visible="false"
-        :style="{ height: scrollHeight + 'px' }"
-        v-if="filteredOptions.length > 0"
-      >
-        <q-card-section>
-          <q-list bordered separator>
-            <q-item
-              v-for="option in filteredOptions"
-              :key="option"
-              clickable
-              v-ripple
-            >
-              <q-item-section side>
-                <q-icon name="place" color="primary" size="md" rounded />
-              </q-item-section>
-              <q-item-section @click="handleSelect(option)" v-close-popup>
-                <q-item-label>{{ option }}</q-item-label>
-                <q-item-label caption>{{ option }} için şehir adı</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-card-section>
-      </q-scroll-area>
+      <q-card-section>
+        <q-list bordered separator>
+          <q-item
+            v-for="option in filteredOptions"
+            :key="option"
+            clickable
+            v-ripple
+          >
+            <q-item-section side>
+              <q-icon name="place" color="primary" size="md" rounded />
+            </q-item-section>
+            <q-item-section @click="handleSelect(option)" v-close-popup>
+              <q-item-label>{{ option }}</q-item-label>
+              <q-item-label caption>{{ option }} için şehir adı</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section>
     </q-card>
-  </q-card>
+  </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted, onBeforeUnmount } from 'vue';
+import { ref, computed } from 'vue';
 
 const myFilter = ref(null);
 const myOptions = ref([
@@ -88,45 +85,46 @@ const myOptions = ref([
   'Option 20',
 ]);
 
-const fontSize = computed(() => {
-  if (myFilter.value == null) return 12;
-  return myFilter.value.length == 0 ? 12 : 25;
-});
-
 const filteredOptions = computed(() => {
+  // console.log(quasarSearchInput.value);
+
   if (!myFilter.value) return myOptions.value;
   return myOptions.value.filter((option) =>
     option.toLowerCase().includes(myFilter.value.toLowerCase())
   );
 });
 
-const scrollHeight = ref(0);
-
-const updateScrollHeight = () => {
-  scrollHeight.value = 500;
-  /*
-  const viewportHeight = window.innerHeight;
-  const otherElementsHeight = document.querySelector('.q-pa-sm').offsetHeight;
-  scrollHeight.value = viewportHeight - otherElementsHeight - 200; // 60 is an arbitrary value for padding/margin
-  */
-};
-
-onMounted(() => {
-  updateScrollHeight();
-  window.addEventListener('resize', updateScrollHeight);
-});
-
-onBeforeUnmount(() => {
-  window.removeEventListener('resize', updateScrollHeight);
-});
-
 const handleSelect = (option) => {
-  console.log('handleSelect', option);
   stationInfo.value = option;
 };
-// const stationInfo = ref('boşşş');
 
 const stationInfo = defineModel('stationInfo', { type: String, default: null });
+
+const quasarSearchInput = ref(null);
 </script>
 
-<style scoped></style>
+<style scoped>
+.big-text {
+  font-size: 25px;
+}
+
+.dialog-content {
+  display: flex;
+  flex-direction: column;
+  height: 100%; /* Bütün pencere yüksekliği */
+}
+
+.first-card {
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  background-color: white; /* İstenirse arka plan rengi */
+  margin-bottom: 10px; /* İhtiyaca bağlı olarak ayarlanabilir */
+}
+
+.second-card {
+  flex: 1; /* Tüm alanı kaplasın */
+  overflow-y: auto; /* İçeriği fazla olursa scrollbar göster */
+  padding-bottom: 100px;
+}
+</style>
